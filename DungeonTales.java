a3798e5206046b0d41d0ded464cdc66ee9e6c1ae
JustinGranof <justin.granofsky@gmail.com>
2016-12-07 17:05:13 -0500

    Update DungeonTales.java

--- DungeonTales.java
+++ DungeonTales.java
@@ -1,25 +1,26 @@
-import java.awt.Dimension;
-import java.awt.Toolkit;
+/*
+DungeonTales.java
+Justin, Alex, and Jerry
+12/7/2016
+Fun 2D platform game
+ */
 
 import javax.swing.JFrame;
 
-
 public class DungeonTales extends JFrame {
-	
-	public DungeonTales(){
-		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		setSize(dim.width/2 + 200, dim.height/2 + 200);
-		setLocation(dim.width/2 - getWidth()/2, dim.height/2 - getHeight()/2);
-		setTitle("| Dungeon Tales |");
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-	}
+
+    public DungeonTales(){
+        setExtendedState(MAXIMIZED_BOTH);
+        setTitle("| Dungeon Tales |");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new DungeonTales();
-	}
+    public static void main(String[] args) {
+        // Activate the constructor, and create the frame.
+        new DungeonTales();
+    }
 
 }

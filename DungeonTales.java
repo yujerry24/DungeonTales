0edc67ab4de453745bd3827da1abe4eed86e020e
JustinGranof <justin.granofsky@gmail.com>
2016-12-07 11:18:17 -0500

    Update DungeonTales.java

--- DungeonTales.java
+++ DungeonTales.java
@@ -1,15 +1,25 @@
-/*
-DungeonTales.java
-12/7/2016
-By Justin, Jerry, and Alex
-A fun 2D platform game
-*/
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
-public class DungeonTales {
+import javax.swing.JFrame;
+
+
+public class DungeonTales extends JFrame {
+	
+	public DungeonTales(){
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		setSize(dim.width/2 + 200, dim.height/2 + 200);
+		setLocation(dim.width/2 - getWidth()/2, dim.height/2 - getHeight()/2);
+		setTitle("| Dungeon Tales |");
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new DungeonTales();
 	}
 
 }

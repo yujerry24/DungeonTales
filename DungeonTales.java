8628bc0bf2de1ea1e733ba4938466ba099b84476
JustinGranof <justin.granofsky@gmail.com>
2016-12-07 11:10:11 -0500

    Added main

--- DungeonTales.java
+++ DungeonTales.java
@@ -1,1 +1,15 @@
+/*
+DungeonTales.java
+12/7/2016
+By Justin, Jerry, and Alex
+A fun 2D platform game
+*/
 
+public class DungeonTales {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

81febcb9b6102b9b7208124b38d05bdda582ee24
Justin <justin.granofsky@gmail.com>
2016-12-09 13:43:44 -0500

    Added Player Object

--- DungeonTales.java
+++ DungeonTales.java
@@ -2,10 +2,43 @@ import javax.swing.JFrame;
 
 public class DungeonTales extends JFrame {
 
+	class Player {
+		private int x;
+		private int y;
+		private String name;
+		
+		public Player(String name){
+			x = 0;
+			y = 0;
+			this.name = name;;
+		}
+		
+		public String getName(){
+			return name;
+		}
+		
+		public int getX(){
+			return this.x;
+		}
+		
+		public int getY(){
+			return this.y;
+		}
+		
+		public void setX(int x){
+			this.x = x;
+		}
+		
+		public void setY(int y){
+			this.y = y;
+		}
+		
+	}
+	
     public DungeonTales(){
         setExtendedState(MAXIMIZED_BOTH);
         setTitle("| Dungeon Tales |");
-        
+                
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
@@ -15,5 +48,5 @@ public class DungeonTales extends JFrame {
         // Activate the constructor, and create the frame.
         new DungeonTales();
     }
-
+    
 }

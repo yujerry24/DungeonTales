cd68a6d21bbbb6d5516bc19508b9081651f107e8
JustinGranof <justin.granofsky@gmail.com>
2016-12-12 11:23:22 -0500

    Update DungeonTales.java

--- DungeonTales.java
+++ DungeonTales.java
@@ -1,8 +1,10 @@
-import java.applet.AudioClip;
+
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -13,7 +15,9 @@ import javax.swing.JFrame;
 
 public class DungeonTales extends JFrame {
 
-	class Player {
+	static Player p;
+	
+	static class Player {
 		private int x;
 		private int y;
 		private String name;
@@ -56,23 +60,19 @@ public class DungeonTales extends JFrame {
 		setSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
 		setResizable(false);
 		setTitle("| Dungeon Tales |");
-
+		
 		try {
 			playMusicFile("MenuMusic.wav", true);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (LineUnavailableException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (UnsupportedAudioFileException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
+	
+	static Clip clip;
 
 	public static void playMusicFile(String file, boolean loop)
 			throws IOException, LineUnavailableException,
@@ -80,7 +80,7 @@ public class DungeonTales extends JFrame {
 		File sound = new File(file);
 
 		AudioInputStream stream = AudioSystem.getAudioInputStream(sound);
-		Clip clip = AudioSystem.getClip();
+		clip = AudioSystem.getClip();
 		clip.open(stream);
 		if (loop) {
 			clip.loop(Clip.LOOP_CONTINUOUSLY);
@@ -89,8 +89,63 @@ public class DungeonTales extends JFrame {
 		}
 
 	}
+	
+	public static void stopMusicFile() throws LineUnavailableException{
+		if(clip == null){
+			return;
+		}
+		clip.stop();
+		clip.close();
+	}
+	
+	public static String getFileValue(String path){
+		int index = path.indexOf(":");
+        
+		String value = "";
+		
+        if(index == -1){
+          // Hint is not found.
+          System.out.println("[ERROR] No category found!");
+          return null;
+        }
+               
+        // The hint or category is equal to the rest of the string from the colon.
+        value = path.substring(index + 1);
+        
+        value.trim();
+        
+        return value;
+	}
 
-	public static void main(String[] args) {
+	public static boolean hasData(){
+		if(p == null){
+			return false;
+		}else{
+			return true;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {	
+		
+		// Create a save file if it does not already exist.
+		File save = new File("save.txt");
+		if(!save.exists()){
+			save.createNewFile();
+			PrintWriter out = new PrintWriter(save);
+			out.println("- DUNGEON TALES SAVE FILE -");
+			out.close();
+		}else{
+			Scanner input = new Scanner(save);
+			while(input.hasNext()){
+				String line = input.nextLine();
+				if(line.indexOf("Player Name:") != -1){
+					String name = getFileValue(line);
+					p = new Player(name);
+				}
+			}
+			input.close();
+		}
+		
 		// Activate the constructor, and create the frame.
 		new DungeonTales();
 	}
